a3f7942d5cfcdabdb9849102eaf4e65ad84cb4b4
Valter Simões <valterrsimoes@gmail.com>
2020-04-24 16:57:13 +0100

    DropDown genero;

--- app/src/main/java/com/example/projeto_final/DisplayCreate.java
+++ app/src/main/java/com/example/projeto_final/DisplayCreate.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayCreate extends AppCompatActivity {
 
@@ -14,5 +20,32 @@ public class DisplayCreate extends AppCompatActivity {
         setContentView(R.layout.activity_display_create);
 
         Intent intentcriar = getIntent();
+
+
+    // ---------- Spinner de seleção do género ----------   https://www.youtube.com/watch?v=4xKsWNmULr0
+        Spinner dropdowngenero;
+        dropdowngenero = (Spinner) findViewById(R.id.spinnergenero);
+
+        final List<String> genero = new ArrayList<>();
+        genero.add(getString(R.string.GeneroF));
+        genero.add(getString(R.string.GeneroM));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String >(this, android.R.layout.simple_spinner_item, genero);
+
+        dropdowngenero.setAdapter(adapter);
+
+        dropdowngenero.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String strGeneroInserido = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    //---------------------------------------------
+
     }
 }
