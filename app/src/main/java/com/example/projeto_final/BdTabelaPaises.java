2b87105332b43711382614524b44686cc36cc45d
Valter Simões <valterrsimoes@gmail.com>
2020-06-11 15:12:05 +0100

    Alteração na Tabela dos paises

--- app/src/main/java/com/example/projeto_final/BdTabelaPaises.java
+++ app/src/main/java/com/example/projeto_final/BdTabelaPaises.java
@@ -8,11 +8,6 @@ import android.provider.BaseColumns;
 public class BdTabelaPaises implements BaseColumns {
 
     public static final String NOME_TABELA_PAISES = "pais";
-    public static final String NUMERO_INFETADOS = "numeroInfetados";
-    public static final String NUMERO_OBITOS = "numeroObitos";
-    public static final String NUMERO_RECUPERADOS = "numeroRecuperados";
-    public static final String NUMERO_CRITICO = "numeroCritico";
-    public static final String NUMERO_DOENTES_CRONICOS = "numeroDoentesCronicos";
     public static final String NUMERO_POPULACAO = "numeroPopulacao";
 
     private final SQLiteDatabase db;
@@ -25,12 +20,7 @@ public class BdTabelaPaises implements BaseColumns {
         db.execSQL(
                 "CREATE TABLE " + NOME_TABELA_PAISES + "(" +
                         _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        NUMERO_POPULACAO + " INTEGER," +
-                        NUMERO_INFETADOS + " INTEGER," +
-                        NUMERO_OBITOS + " INTEGER," +
-                        NUMERO_RECUPERADOS + " INTEGER," +
-                        NUMERO_CRITICO + " INTEGER," +
-                        NUMERO_DOENTES_CRONICOS + " INTEGER" +
+                        NUMERO_POPULACAO + " INTEGER" +
                         ")");
     }
 
